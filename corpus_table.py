83f42d6cd512e938a8046222eb8317aac4f695e0
daan-282421908 <75156771+daan-282421908@users.noreply.github.com>
2024-03-16 20:46:24 +0800

    Update corpus_table.py

--- corpus_table.py
+++ corpus_table.py
@@ -3,85 +3,23 @@ import re
 import csv
 from nltk import sent_tokenize
 
-
-def get_pos(file_path):
+def pos(file_path):
     with open(file_path,'r') as f:
-        title = f.readline()
-    with open(file_path,'r') as f:
-        abstract = f.readline()
+        title = f.read()
        
     if asf == '\n':
         asf = src.readline() 
-
     title = title.replace('\n','')
-    abstract = abstract.replace('\n','')
-    list.append(title)
-    list.extend(sent_tokenize(abstract))
-    
-    for i,s in enumerate(list) :
-        ids = re.split('([ -/().,:;])',s)
-        for j,w in enumerate(ids) :
-            if w != ' ' and w != '':
-                position_dic_word[position] = w
-                if i != 0 :
-                    if j == len(words)-2 :
-                        end.add(position)
-                else :
-                    if j >= len(words)-4 and w == '.' :
-                        position_dic_end.add(position)
-            position += len(w)
-        position += 1
-        
-    return position_of_word,position_of_end
+    return title
 
 def get_event_info(file_path,position_of_word):
     with open(file_path,'r') as f:
         event_notation = dict()
         event_index = dict()
-        
         line = src.readline()
-      
-        while line:
-            if line[0] != 'T' :
-                line = src.readline()
-                continue
-          
-            words = re.split('[ \t]',line)
-            index = words[0]
-            type_ = words[1]
-            start = int(words[2])
-            end = int(words[3])
-    
-            i = 0
-            while position2word[i+1][0] <= start:
-                i += 1
-            start = position2word[i][0]
-    
-            event_list = []
-            for p2w in position2word:
-                p = p2w[0]
-                if ( p >= start and p < end ) :
-                event_list.append(p)
+    return line
 
-            if len(event_list) == 1 :
-                event = event_list[0]
-                event_notation[event] = type_ + '-Unit'
-            else :
-                event = event_list[0]
-                event_notation[event] = type_ + '-Begin'
-                event = event_list[-1]
-                event_notation[event] = type_ + '-Last'
-                for i in range(1,len(event_list)-1):
-                    event = event_list[i]
-                    event_notation[event] = type_ + '-Inside'
-            for event in event_list :
-                event_index[event] = index
-        line = f.readline()
-    return event_notation, event_index
-
-txt_path = os.path.abspath('.') + '/txt' 
-
-for fpath,_,files in os.walk(txt_path):
+for fpath,_,files in os.walk(path):
     for fl in files:
         file_path = os.path.join(fpath,fl)
         position_of_word,position_of_end = get_pos(file_path)
@@ -98,35 +36,3 @@ for fpath,_,files in os.walk(txt_path):
         file_path = file_path.replace('/a2', '/table')
         file_path = file_path.replace('.a2', '.csv')
 
-        with open(file_path,'w') as f:
-            writer = csv.writer(csvfile)
-            writer.writerow(['position', 'word', 'entity_index', 'entity_notation','event_index', 'event_notation', 'is_end'])
-
-            csvfile = open(file_path,'a+')
-            writer = csv.writer(csvfile)
-            
-            for p2w in position2word:
-
-                p = p2w[0]
-                w = p2w[1]
-
-                if p in entity_index:
-                    e_i = entity_index[p]
-                    e_n = entity_notation[p]
-                else:
-                    e_i, e_n = '',''
-
-                if p in event_index:
-                    ev_i = event_index[p]
-                    ev_n = event_notation[p]
-                else:
-                    ev_i,ev_n = '',''
-
-                if p in position2end:
-                    flag = True
-                else:
-                    flag = False
-                    
-                writer.writerow([p, w, e_i, e_n, ev_i, ev_n, flag])
-                
-            csvfile.close()
